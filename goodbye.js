9cc932e943f6df2b10537c86018cdfd82b2b616f
Matthew Wayock <wayock@gmail.com>
2019-05-24 15:29:19 -0400

    questions 1-6 completed

---
+++ goodbye.js
@@ -0,0 +1,14 @@
+exports.goodbyeEnglish = function(){
+  console.log('goodbye');
+}
+
+exports.goodbyeJapanese = function(){
+  console.log('sayōnara');
+}
+exports.goodbyeSpanish = function(){
+  console.log('adiós');
+}
+
+exports.goodbyeIcelandic = function(){
+  console.log('bless');
+}
--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const greetings = require("./greeting.js");
+const goodbyes = require("./goodbye.js")
 
 greetings.greetingEnglish();
 greetings.greetingJapanese();
 greetings.greetingSpanish();
 greetings.greetingIcelandic();
+
+goodbyes.goodbyeEnglish();
+goodbyes.goodbyeJapanese();
+goodbyes.goodbyeSpanish();
+goodbyes.goodbyeIcelandic();
